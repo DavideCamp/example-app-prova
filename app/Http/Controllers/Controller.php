b3232d60dcecc4ce0ba19e1dd45702504d3dbde6
Davide Campana <davidecampana@MacBook-Pro.station>
2022-06-17 14:58:41 +0200

    initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Url;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Client\Request;
+
+class Controller extends BaseController
+{
+
+
+
+        
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+       
+    </head>
+    <body>
+
+
+
+
+        
+
+
+        <div class="container">
+
+                <h1>Short url</h1>
+
+                @if(session('success_message'))
+                    {!!session('success_message')!!}
+                @endif
+            <div class="mb-3">
+                <form method="POST" action="{{route('short.url')}}">
+                @csrf
+                    <input type="url" name="original_url"/>
+
+                    
+                    <button type="submit"class="btn btn-success" >
+                        accorciami
+                    </button>
+
+
+
+                </form>
+            </div>
+                    
+                    
+                </div>
+           
+        </div>
+    </body>
+</html>
